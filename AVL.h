3e64e66030787ff47f633f69c95e2e6da548c690
pandapythoner <stepanov.aa@students.sch2.ru>
2022-01-07 20:11:32 +0300

    Initial commit

---
+++ AVL.h
@@ -0,0 +1,341 @@
+#ifndef AVL_AVL_H
+#define AVL_AVL_H
+#include <utility>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+
+template<class Key>
+class AVL{
+public:
+
+    static int max(int a, int b){
+        if(a < b){
+            return b;
+        }
+        return a;
+    }
+
+    template<class NodeKey>
+    class node{
+    public:
+
+        typedef node* pnode;
+
+        NodeKey key;
+        pnode l = nullptr;
+        pnode r = nullptr;
+        int h = 1;
+
+        node(const NodeKey &key) : key(key), l(nullptr), r(nullptr), h(1) {
+
+        }
+
+
+        int get_height_slow(){
+            int res = 1;
+            if(l != nullptr){
+                if(l->key > key){
+                    // throw "Bebra...";
+                }
+                res = max(res, l->get_height_slow() + 1);
+            }
+            if(r != nullptr) {
+                if(key > r->key){
+                    //  "Bebra...";
+                }
+                res = max(res, r->get_height_slow() + 1);
+            }
+            return res;
+        }
+
+
+        int get_height(const pnode &p){
+            if(p == nullptr){
+                return 0;
+            }
+            return p->h;
+        }
+
+
+        inline int get_left_height(){
+            return get_height(l);
+        }
+
+
+        inline int get_right_height(){
+            return get_height(r);
+        }
+
+
+        void update(){
+            h = max(get_left_height(), get_right_height()) + 1;
+        }
+
+
+
+
+    };
+
+    typedef node<Key>* pnode;
+
+
+    pnode rotate_right(pnode y){
+        pnode x = y->l;
+        y->l = x->r;
+        x->r = y;
+        y->update();
+        x->update();
+        return x;
+    }
+
+
+    pnode rotate_left(pnode x){
+        pnode y = x->r;
+        x->r = y->l;
+        y->l = x;
+        x->update();
+        y->update();
+        return y;
+    }
+
+
+    pnode balance(pnode v){
+        int lh = v->get_left_height();
+        int rh = v->get_right_height();
+        int balance = (lh - rh);
+        if(lh + 2 == rh){
+            if(v->r->get_left_height() > v->r->get_right_height()){
+                v->r = rotate_right(v->r);
+                v->update();
+            }
+            return rotate_left(v);
+        } else if(lh == rh + 2){
+            if(v->l->get_left_height() < v->l->get_right_height()){
+                v->l = rotate_left(v->l);
+                v->update();
+            }
+            return rotate_right(v);
+        }
+        return v;
+    }
+
+
+    pnode insert(pnode v, pnode d){
+        if(v == nullptr){
+            return d;
+        }
+        if(v->key <= d->key){
+            v->r = insert(v->r, d);
+            v->update();
+            return balance(v);
+        } else{
+            v->l = insert(v->l, d);
+            v->update();
+            return balance(v);
+        }
+    }
+
+
+    pnode find(pnode v, const Key &key){
+        while(v != nullptr && v->key != key){
+            if(key < v->key){
+                v = v->l;
+            } else{
+                v = v->r;
+            }
+        }
+        return v;
+    }
+
+
+    pnode find_min(pnode v){
+        while(v != nullptr && v->l != nullptr){
+            v = v->l;
+        }
+        return v;
+    }
+
+
+    std::pair<pnode, pnode> delete_min(pnode v){
+        if(v == nullptr){
+            return std::make_pair(v, v);
+        }
+        if(v->l == nullptr){
+            auto vr = v->r;
+            v->r = nullptr;
+            v->update();
+            return std::make_pair(vr, v);
+        }
+        auto x = delete_min(v->l);
+        v->l = x.first;
+        v->update();
+        v = balance(v);
+        x.first = v;
+        return x;
+    }
+
+
+    std::pair<pnode, pnode> delete_by_key(pnode v, const Key &key){
+        if(v == nullptr){
+            return std::make_pair(v, v);
+        }
+        if(v->key > key){
+            auto x = delete_by_key(v->l, key);
+            v->l = x.first;
+            v->update();
+            x.first = balance(v);
+            return x;
+        }
+        if(v->key < key){
+            auto x = delete_by_key(v->r, key);
+            v->r = x.first;
+            v->update();
+            x.first = balance(v);
+            return x;
+        }
+        auto vl = v->l;
+        auto vr = v->r;
+        v->l = nullptr;
+        v->r = nullptr;
+        if(vr == nullptr){
+            return std::make_pair(vl, v);
+        }
+        auto x = delete_min(vr);
+        auto u = x.second;
+        u->l = vl;
+        u->r = x.first;
+        u->update();
+        u = balance(u);
+        return std::make_pair(u, v);
+    }
+
+
+
+    int to_strings(pnode v, std::vector<std::string> &res, std::string (*to_string)(Key)){
+        res.clear();
+        if(v == nullptr){
+            res = {"   "};
+            return 0;
+        }
+        if(v->l == nullptr && v->r == nullptr){
+            res.push_back(to_string(v->key));
+            res[0].push_back(' ');
+            res[0].push_back(' ');
+            res[0].push_back(' ');
+            return 0;
+        }
+        std::vector<std::string> x, y;
+        int i = to_strings(v->l, x, to_string);
+        int j = to_strings(v->r, y, to_string);
+        j += x[0].size();
+        while(x.size() < y.size()){
+            x.push_back("");
+            while(x.back().size() < x[x.size() - 2].size()){
+                x.back().push_back(' ');
+            }
+        }
+        while(y.size() < x.size()){
+            y.push_back("");
+            while(y.back().size() < y[y.size() - 2].size()){
+                y.back().push_back(' ');
+            }
+        }
+        int sz = x[0].size() + y[0].size();
+        while(i + 1 < j){
+            res.push_back("");
+            for(int c = 0; c < sz; ++c){
+                if(c == i && v->l != nullptr){
+                    res.back().push_back('/');
+                } else if(c == j && v->r != nullptr){
+                    res.back().push_back('\\');
+                } else{
+                    res.back().push_back(' ');
+                }
+            }
+            i += 1;
+            j -= 1;
+        }
+        std::string a = to_string(v->key);
+        res.push_back("");
+        for(int c = 0; c < i; ++c){
+            res.back().push_back(' ');
+        }
+        for(int c = 0; c < a.size(); ++c){
+            res.back().push_back(a[c]);
+        }
+        res.back().push_back(' ');
+        sz = max(sz, res.back().size());
+        reverse(res.begin(), res.end());
+        for(int c = 0; c < x.size(); ++c){
+            res.push_back(x[c] + y[c]);
+        }
+        for(int c = 0; c < res.size(); ++c){
+            res[c].resize(sz, ' ');
+        }
+        return i;
+    }
+
+
+
+
+    pnode root = nullptr;
+
+
+    void insert(const Key &k){
+        pnode v = new node<Key>(k);
+        root = insert(root, v);
+    }
+
+
+    int checkout_height(){
+        if(root == nullptr) {
+            return 0;
+        }
+        return root->get_height_slow();
+    }
+
+
+    pnode find(const Key &key){
+        return find(root, key);
+    }
+
+
+    pnode find_min(){
+        return find_min(root);
+    }
+
+
+    void delete_min(){
+        auto x = delete_min(root);
+        root = x.first;
+        if(x.second != nullptr){
+            delete x.second;
+        }
+    }
+
+
+    void delete_by_key(const Key &key){
+        auto x = delete_by_key(root, key);
+        root = x.first;
+        if(x.second != nullptr){
+            delete x.second;
+        }
+    }
+
+};
+
+template<class T>
+std::ostream& operator<<(std::ostream &out, AVL<T> &a){
+    std::vector<std::string> s;
+    a.to_strings(a.root, s, std::to_string);
+    for(auto &x: s){
+        out << x << "\n";
+    }
+    return out;
+}
+
+
+#endif //AVL_AVL_H
---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include "AVL.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <cassert>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+
+#define int long long
+
+
+using namespace std;
+
+
+mt19937 rnd(123123);
+
+
+void help(){
+    cout << "\n\n";
+    cout << "This is a program, in that you can play with AVL tree" << "\n";
+    cout << "Made by Anton Stepanov (all rights reserved)" << "\n\n";
+    cout << "Commands: " << "\n";
+    cout << "insert x (inserts node with key x)" << "\n";
+    cout << "find x (finds node with key x)" << "\n";
+    cout << "findmin (finds node with minimal key)" << "\n";
+    cout << "del x (deletes node with key x)" << "\n";
+    cout << "delmin (deletes node minimal key)" << "\n";
+    cout << "print (prints AVL tree)" << "\n";
+    cout << "help (shows information about this program)" << "\n";
+    cout << "exit (stops program)" << "\n";
+    cout << "\n\n";
+}
+
+
+int32_t main() {
+    AVL<int> a;
+    help();
+    while(1){
+        string s;
+        cin >> s;
+        if(s == "insert"){
+            int x;
+            cin >> x;
+            a.insert(x);
+        } else if(s == "find"){
+            int x;
+            cin >> x;
+            auto p = a.find(x);
+            if(p == nullptr){
+                cout << -1 << "\n";
+            } else{
+                cout << p->key << "\n";
+            }
+        } else if(s == "findmin"){
+            auto p = a.find_min();
+            if(p == nullptr){
+                cout << -1 << "\n";
+            } else{
+                cout << p->key << "\n";
+            }
+        } else if(s == "delmin"){
+            a.delete_min();
+        } else if(s == "del"){
+            int x;
+            cin >> x;
+            a.delete_by_key(x);
+        } else if(s == "print"){
+            cout << a << "\n";
+        } else if(s == "exit"){
+            break;
+        } else if(s == "help"){
+            help();
+        }
+    }
+    return 0;
+}
